bbe51c7e8f28c7d4cbd803b01bd58b662a91cdfb
AmrAbrazek <amr.3brazek@gmail.com>
2023-10-14 20:37:58 +0300

    Implement Post views including list create update and delete

--- pyramids/post/views.py
+++ pyramids/post/views.py
@@ -1,3 +1,35 @@
+from .models import Post
 from django.shortcuts import render
-
+from rest_framework import generics
+from .serializers import PostSerializer, CreatePostSerializer
+from .permissions import IsAuthorOrReadOnly
+from rest_framework.permissions import (
+    AllowAny,
+    IsAuthenticated,
+)
 # Create your views here.
+
+# ListAPIView
+class Post_list(generics.ListAPIView):
+    permission_classes = [AllowAny]
+    queryset = Post.objects.all().order_by("-created_at")
+    serializer_class = PostSerializer
+
+# CreateAPIView
+class PostCreate(generics.CreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = CreatePostSerializer
+    permission_classes = [AllowAny, IsAuthenticated]
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+class PostUpdate(generics.UpdateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = CreatePostSerializer
+    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
+    # permission_classes = [AllowAny]
+
+class PostDelete(generics.DestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
