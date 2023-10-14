6f5d784ab4722ca0dc3835a704946ca83c5b80fb
AmrAbrazek <amr.3brazek@gmail.com>
2023-10-14 20:24:58 +0300

    Add post model

--- pyramids/post/models.py
+++ pyramids/post/models.py
@@ -1,3 +1,9 @@
+from user.models import User
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255, null=True, blank=True)    
+    content = models.TextField(null=True)  
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="author_posts")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
